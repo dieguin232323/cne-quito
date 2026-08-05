398278759ae9dfcb7b6fd274858fa122899a9cb4
Belent06 <belentashiguano@gmail.com>
2026-08-05 14:12:55 -0500

    feat: :white_check_mark: Actualización de Pestañas
    Se actualizaron las pestañas para que los demás compañeros trabajen en ellas.

---
+++ app/(tags)/home.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { colors } from "../../theme/colors";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.title}>Pestaña Inicio (Home)</Text>
+        <Text style={styles.subtitle}>hagaan</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.fondo,
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: colors.morado,
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: colors.textoSuave,
+  },
+});
